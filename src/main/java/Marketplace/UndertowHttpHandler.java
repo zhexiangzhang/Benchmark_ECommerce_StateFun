5e7871890b8ec7f1e4f96b7ca7e2f0027d1c08cd
zhexiangzhang <zhexiangzhang@163.com>
2023-09-17 11:05:11 +0200

    add productUpdate

--- src/main/java/Marketplace/UndertowHttpHandler.java
+++ src/main/java/Marketplace/UndertowHttpHandler.java
@@ -24,7 +24,6 @@ public final class UndertowHttpHandler implements HttpHandler {
 
     private void onRequestBody(HttpServerExchange exchange, byte[] requestBytes) {
         try {
-            System.out.println("requestBytes: " + requestBytes);
             CompletableFuture<Slice> future = handler.handle(Slices.wrap(requestBytes));
             exchange.dispatch();
             future.whenComplete(
